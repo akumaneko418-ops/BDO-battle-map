bcedff8684606126ab8a26af2b5aa975b482b062
akumaneko418-ops <akumaneko418@gmail.com>
2026-05-15 13:50:39 +0900

    Update server.js

--- server.js
+++ server.js
@@ -232,3 +232,9 @@ io.on("connection", socket => {
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
+- app.use(express.json());
++ app.use(express.json({ limit: "50mb" }));
+
+- app.use(express.urlencoded({ extended: true }));
++ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
